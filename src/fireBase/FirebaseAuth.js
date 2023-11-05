325a6d8de2523f5cfde16d43656894ce559f8648
Subhash Sharma <67090987+SubhashSharma810@users.noreply.github.com>
2023-11-05 14:41:18 +0530

    deploying

--- src/fireBase/FirebaseAuth.js
+++ src/fireBase/FirebaseAuth.js
@@ -5,11 +5,12 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL:process.env.REACT_APP_DATABASEURL,
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID
+  // measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
